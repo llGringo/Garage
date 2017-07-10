923bb2af4588e45f0b94877e9c7311175f87d02b
llGringo <dacosta.fabio82@gmail.com>
2017-07-10 18:01:06 +0200

    Ajout classe Garage, Vehicule et Diagramme a implementer

---
+++ src/Garage.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class Garage {
+	protected List<Vehicule> voiture = (List<Vehicule>) new Vehicule();
+	
+
+}
---
+++ src/Vehicule.java
@@ -0,0 +1,6 @@
+
+public class Vehicule extends Garage{
+	protected Double prix = null;
+	protected String nom = "";
+	
+}
